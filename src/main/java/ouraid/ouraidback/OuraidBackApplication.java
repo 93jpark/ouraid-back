fd8d5e4ab681578fe786e96f475aebe9d96f3175
Joonwoo Park <93jpark@gmail.com>
2022-07-30 15:40:27 +0900

    first commit

---
+++ src/main/java/ouraid/ouraidback/OuraidBackApplication.java
@@ -0,0 +1,13 @@
+package ouraid.ouraidback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OuraidBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OuraidBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ouraid/ouraidback/OuraidBackApplicationTests.java
@@ -0,0 +1,13 @@
+package ouraid.ouraidback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OuraidBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
