17d9ab4c66efbc644b20c152b0228b12c142f502
Joonwoo Park <93jpark@gmail.com>
2022-08-26 16:34:25 +0900

    implement creation method

--- src/main/java/ouraid/ouraidback/domain/Character.java
+++ src/main/java/ouraid/ouraidback/domain/Character.java
@@ -12,54 +12,67 @@ import javax.persistence.*;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Character {
 
-    public Character(Server server, MainClass mainClass, SubClass subClass, Float ability, Member characterOwner) {
-        this.server = server;
-        this.mainClass = mainClass;
-        this.subClass = subClass;
-        this.ability = ability;
-        this.characterOwner = characterOwner;
+    @Id @GeneratedValue @Column(name="character_id") private Long id;
+
+    @Enumerated(EnumType.STRING) @NotNull private Server server;
+
+    @Enumerated(EnumType.STRING) @NotNull private MainClass mainClass;
+
+    @Enumerated(EnumType.STRING) @NotNull private SubClass subClass;
+
+    @NotNull private Float ability;
+
+    @ManyToOne @JoinColumn(name = "member_id") private Member characterOwner;
+
+    @ManyToOne @JoinColumn(name = "guild_id") private Guild joinedGuild;
+
+    @ManyToOne @JoinColumn(name = "community_id") private Community joinedCommunity;
+
+    /**
+     * create character without any joined guild or community
+     * @param server
+     * @param mainClass
+     * @param subClass
+     * @param ability
+     * @param characterOwner
+     * @return
+     */
+    public Character create(Server server, MainClass mainClass, SubClass subClass, Float ability, Member characterOwner) {
+        Character character = new Character();
+        character.server = server;
+        character.mainClass = mainClass;
+        character.subClass = subClass;
+        character.ability = ability;
+        character.characterOwner = characterOwner;
+
+        return character;
     }
 
-    public Character(Server server, MainClass mainClass, SubClass subClass, Float ability, Member characterOwner, Guild joinedGuild, Community joinedCommunity) {
-        this.server = server;
-        this.mainClass = mainClass;
-        this.subClass = subClass;
-        this.ability = ability;
-        this.characterOwner = characterOwner;
-        this.joinedGuild = joinedGuild;
-        this.joinedCommunity = joinedCommunity;
+    /**
+     * create character with joined guild and community
+     * @param server
+     * @param mainClass
+     * @param subClass
+     * @param ability
+     * @param characterOwner
+     * @param joinedGuild
+     * @param joinedCommunity
+     * @return
+     */
+    public Character create(Server server, MainClass mainClass, SubClass subClass, Float ability, Member characterOwner, Guild joinedGuild, Community joinedCommunity) {
+        Character character = new Character();
+
+        character.server = server;
+        character.mainClass = mainClass;
+        character.subClass = subClass;
+        character.ability = ability;
+        character.characterOwner = characterOwner;
+        character.joinedGuild = joinedGuild;
+        character.joinedCommunity = joinedCommunity;
+
+        return character;
     }
 
-    @Id @GeneratedValue
-    @Column(name="character_id")
-    private Long id;
 
-    @Enumerated(EnumType.STRING)
-    @NotNull
-    private Server server;
-
-    @Enumerated(EnumType.STRING)
-    @NotNull
-    private MainClass mainClass;
-
-    @Enumerated(EnumType.STRING)
-    @NotNull
-    private SubClass subClass;
-
-    @NotNull
-    private Float ability;
-
-    @ManyToOne
-    @JoinColumn(name = "member_id")
-    private Member characterOwner;
-
-    @ManyToOne
-    @JoinColumn(name = "guild_id")
-    private Guild joinedGuild;
-
-
-    @ManyToOne
-    @JoinColumn(name = "community_id")
-    private Community joinedCommunity;
 
 }
