381a303f66ae97c86847ce6226c543c5d7361e17
Joonwoo Park <93jpark@gmail.com>
2022-08-26 16:34:57 +0900

    implements creation method

--- src/main/java/ouraid/ouraidback/domain/Community.java
+++ src/main/java/ouraid/ouraidback/domain/Community.java
@@ -18,6 +18,7 @@ public class Community {
     @Column(name="community_id")
     private Long id;
 
+    @NotNull
     private String name;
 
     @Enumerated(EnumType.STRING)
@@ -27,7 +28,7 @@ public class Community {
     @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "community_id")
     @NotNull
-    private Member CommunityMaster;
+    private Member communityMaster;
 
     @OneToMany(mappedBy = "joinedCommunity")
     private List<Member> joinedMembers = new ArrayList<>();
@@ -39,6 +40,28 @@ public class Community {
     private List<Character> joinedCharacters = new ArrayList<>();
 
 
+    /**
+     * basic Community creation
+     * @param server
+     * @param name
+     * @param master
+     * @return
+     */
+    public Community create(Server server, String name, Member master) {
+        Community community = new Community();
+        community.server = server;
+        community.name = name;
+        community.communityMaster = master;
 
+        return community;
+    }
+
+    // 비즈니스 로직
+    /**
+     * 커뮤니티 마스터 변경
+     */
+    public void changeMaster() {
+
+    }
 
 }
