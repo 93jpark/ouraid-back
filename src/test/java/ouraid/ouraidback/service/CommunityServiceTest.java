e18fc52c3ea5cb0c1abb1f2cb2e8d5eefa5bc8ff
Joonwoo Park <93jpark@gmail.com>
2022-08-31 17:36:25 +0900

    implemented basic community cration test code

--- src/test/java/ouraid/ouraidback/service/CommunityServiceTest.java
+++ src/test/java/ouraid/ouraidback/service/CommunityServiceTest.java
@@ -1,11 +1,17 @@
 package ouraid.ouraidback.service;
 
 import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
+import ouraid.ouraidback.domain.Community;
+import ouraid.ouraidback.domain.Member;
+import ouraid.ouraidback.domain.enums.Server;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -13,8 +19,24 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class CommunityServiceTest {
 
-    @Autowired
-    CommunityService communityService;
+    @Autowired CommunityService communityService;
+    @Autowired MemberService memberService;
+    @Autowired CharacterService characterService;
 
+    @Test
+    public void 단일_연합_생성() throws Exception {
+        //given
+        Member newMember = Member.create("유니츠", "93jpark@gmail.com", "12345", Server.SHUSIA);
+        memberService.registerMember(newMember);
+        Community newCom = Community.create(Server.SHUSIA, "void", newMember);
+
+        //when
+        Long comId = communityService.registerCommunity(newCom);
+
+        //then
+        Community findCom = communityService.findByComId(comId);
+        assertEquals(findCom.getName(), "void");
+        assertEquals(findCom.getCommunityMaster().getNickname(), "유니츠");
+    }
 
 }
