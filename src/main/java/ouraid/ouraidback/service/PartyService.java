10a3b374f584ea4049f5ac1ebabcf674b048c2d1
Joonwoo Park <93jpark@gmail.com>
2022-09-23 18:01:48 +0900

    implemented and refactored related with new feature participant status

--- src/main/java/ouraid/ouraidback/service/PartyService.java
+++ src/main/java/ouraid/ouraidback/service/PartyService.java
@@ -7,10 +7,7 @@ import org.springframework.transaction.annotation.Transactional;
 import ouraid.ouraidback.domain.Characters;
 import ouraid.ouraidback.domain.GuildMember;
 import ouraid.ouraidback.domain.Member;
-import ouraid.ouraidback.domain.enums.ParticipantType;
-import ouraid.ouraidback.domain.enums.PartyStatus;
-import ouraid.ouraidback.domain.enums.PartyType;
-import ouraid.ouraidback.domain.enums.RecruitType;
+import ouraid.ouraidback.domain.enums.*;
 import ouraid.ouraidback.domain.party.Party;
 import ouraid.ouraidback.domain.party.PartyParticipant;
 import ouraid.ouraidback.repository.PartyRepository;
@@ -19,9 +16,12 @@ import java.time.Instant;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import static ouraid.ouraidback.domain.enums.ParticipantStatus.ACCEPTED;
+import static ouraid.ouraidback.domain.enums.ParticipantStatus.DECLINED;
 import static ouraid.ouraidback.domain.enums.ParticipantType.HOLDER;
 import static ouraid.ouraidback.domain.enums.ParticipantType.NORMAL;
 import static ouraid.ouraidback.domain.enums.PartyStatus.*;
+import static ouraid.ouraidback.domain.enums.PartyType.ASSIST;
 
 @Service @Slf4j
 @RequiredArgsConstructor
@@ -36,6 +36,8 @@ public class PartyService {
     public void registerParty(Party party) throws Exception {
         partyRepository.registerParty(party);
         this.joinCharacterOnPartyWithType(party.getId(), party.getPartyHolderCharacter().getId(), HOLDER);
+        PartyParticipant pp = partyRepository.findPartyParticipant(party.getId(), party.getPartyHolderCharacter().getId()).get(0);
+        pp.updateStatus(ParticipantStatus.HOLDER);
     }
 
     // 파티 삭제
@@ -44,98 +46,124 @@ public class PartyService {
         partyRepository.removeParty(party);
     }
 
-    // 파티 파티원 추가
+    // 파티에 파티원 추가
     @Transactional
     public void joinCharacterOnParty(Long pId, Long cId) throws Exception {
-        Party findParty = partyRepository.findOne(pId);
+        Party findParty = partyRepository.findOneParty(pId);
         Characters findChar = characterService.findOne(cId);
         Member charOwner = findChar.getCharacterOwner();
 
-        /** 파티 정원 초과인지 확인 */
-        if(validateJoinable(findParty)) {
-            /** 이미 해당 멤버의 캐릭터가 참여되어있는지 확인 필요 */
-            if(partyRepository.findPartyParticipant(pId, cId).isEmpty()) {
-                PartyParticipant pp = PartyParticipant.createPartyParticipant(findParty, charOwner, findChar);
-                pp.designateType(NORMAL);
-                findParty.addPartyCharacter(pp);
-            } else {
-                log.info("{} party already other character who has Member'{}'.", findParty.getId(), charOwner.getNickname());
-                throw new Exception("Duplicated character registered");
-            }
+        /** 이미 해당 멤버의 캐릭터가 참여되어있는지 확인 필요 */
+        if(partyRepository.findPartyParticipantByMember(pId, charOwner.getId()).isEmpty()) {
+            PartyParticipant pp = PartyParticipant.createPartyParticipant(findParty, charOwner, findChar);
+            pp.designateType(NORMAL);
+            findParty.addPartyCharacter(pp);
+            partyRepository.registerPartyParticipant(pp);
         } else {
-            log.info("{} party is already fulled.", findParty.getId());
-            throw new Exception("party is already full");
+            log.info("{} party already other character who has Member'{}'.", findParty.getId(), charOwner.getNickname());
+            throw new Exception("Duplicated character registered");
         }
-
     }
 
-
-    // 파티 파티원 추가
+    // 파티 파티원 추가 + 파티원 타입
     @Transactional
     public void joinCharacterOnPartyWithType(Long pId, Long cId, ParticipantType type) throws Exception {
-        Party findParty = partyRepository.findOne(pId);
+        Party findParty = partyRepository.findOneParty(pId);
+
         Characters findChar = characterService.findOne(cId);
         Member charOwner = findChar.getCharacterOwner();
 
-        /** 파티 정원 초과인지 확인 */
-        if(validateJoinable(findParty)) {
-            /** 이미 해당 멤버의 캐릭터가 참여되어있는지 확인 필요 */
-            if(partyRepository.findPartyParticipant(pId, cId).isEmpty()) {
-                PartyParticipant pp = PartyParticipant.createPartyParticipant(findParty, charOwner, findChar, type);
-                findParty.addPartyCharacter(pp);
-            } else {
-                log.info("{} party already other character who has Member'{}'.", findParty.getId(), charOwner.getNickname());
-                throw new Exception("Duplicated character registered");
-            }
+        /** 이미 해당 멤버의 캐릭터가 참여되어있는지 확인 필요 */
+        if(partyRepository.findPartyParticipantByMember(pId, charOwner.getId()).isEmpty()) {
+            PartyParticipant pp = PartyParticipant.createPartyParticipant(findParty, charOwner, findChar, type);
+            findParty.addPartyCharacter(pp);
+            partyRepository.registerPartyParticipant(pp);
         } else {
-            log.info("{} party is already fulled.", findParty.getId());
-            throw new Exception("party is already full");
+            log.info("{} party already other character who has Member'{}'.", findParty.getId(), charOwner.getNickname());
+            throw new Exception("Duplicated character registered");
+        }
+    }
+
+    // 특정 파티의 파티원 가입 승인
+    @Transactional
+    public void acceptParticipant(Long ppId) throws Exception {
+        PartyParticipant pp = partyRepository.findOneParticipant(ppId);
+        Party party = pp.getJoinedParty();
+        // 파티에 빈 자리가 있는 경우에만 승이
+        if(validateJoinable(party) && party.getPartyStatus() != FULL && party.getPartyStatus() != COMPLETE) {
+            party.acceptParticipant();
+            pp.updateStatus(ACCEPTED);
+        } else {
+            throw new Exception(party.getId()+" party is already full");
+        }
+        // 파티가 가득 찼다면, 파티 상태 변경
+        if(!validateJoinable(party)) {
+            party.updatePartyStatus(FULL);
         }
 
     }
+
+    // 특정 파티의 파티원 가입 거절
+    @Transactional
+    public void declineParticipant(Long ppId) throws Exception {
+        PartyParticipant pp = partyRepository.findOneParticipant(ppId);
+        Party party = pp.getJoinedParty();
+        if(party.getPartyStatus() != COMPLETE) {
+            // 승인되었던 유저라면 파티 잔존인원 수정
+            if(pp.getParticipantStatus()==ACCEPTED) {
+                party.repelAcceptedMember();
+            }
+            if(party.getPartyStatus() == FULL) {
+                party.updatePartyStatus(RECRUIT);
+            }
+            pp.updateStatus(DECLINED);
+        } else {
+            log.info("{} party is already cleared", party.getId());
+            throw new Exception("Already cleared party.");
+        }
+
+
+    }
+
 
     private boolean validateJoinable(Party findParty) {
         int capacity = findParty.getPartyCapacity(); // 정원
-        int partyJoinedMemberNum = findParty.getRegisteredMemberSize(); // 현재인원
+        int acceptedMemberSize = findParty.getAcceptedMemberSize(); // 현재인원
 
-        return capacity > partyJoinedMemberNum ? true : false;
+        return capacity > acceptedMemberSize ? true : false;
     }
 
 
-    // 파티에 파티원 삭제
-    public void expelCharacter(Long pId, Long cId) {
-
-    }
 
     // 파티 상태 변경 - PartyStatus:RECRUIT,FULL,COMPLETE
     public void updatePartyStatus(Long pId, PartyStatus status) {
-        Party findParty = partyRepository.findOne(pId);
+        Party findParty = partyRepository.findOneParty(pId);
         findParty.updatePartyStatus(status);
     }
 
     // 파티 타입 변경 - PartyType:TRIAL, REWARD, CHALLENGE, NORMAL, ASSIST
     public void updatePartyType(Long pId, PartyType type) {
-        Party findParty = partyRepository.findOne(pId);
+        Party findParty = partyRepository.findOneParty(pId);
         findParty.updatePartyType(type);
     }
 
     // 파티 모집 변경 - RecruitType: OPEN, GUILD
     public void updatePartyRecruitType(Long pId, RecruitType type) {
-        Party findParty = partyRepository.findOne(pId);
+        Party findParty = partyRepository.findOneParty(pId);
         findParty.updateRecruitType(type);
     }
 
     // 파티원 타입 지정 - DRIVER, RIDER, NORMAL
     public void updateParticipantType(Long pId, Long cId, ParticipantType type) {
-        Party findParty = partyRepository.findOne(pId);
+        Party findParty = partyRepository.findOneParty(pId);
         PartyParticipant pp = partyRepository.findPartyParticipant(pId, cId).get(0);
         pp.designateType(type);
     }
 
     // 파티 업둥 정원 수정
     public void updatePartyFreeRiderCapacity(Long pId, int capacity) throws Exception {
-        Party findParty = partyRepository.findOne(pId);
-        if(findParty.getPartyType().equals(PartyType.ASSIST)) {
+        Party findParty = partyRepository.findOneParty(pId);
+        if(findParty.getPartyType().equals(ASSIST)) {
             throw new Exception("Non-assist party cannot set free rider capacity.");
         } else {
             if(capacity <= 0) {
@@ -150,20 +178,20 @@ public class PartyService {
         if(ability > 0){
             throw new Exception("ability cannot be negative");
         } else {
-            Party findParty = partyRepository.findOne(pId);
+            Party findParty = partyRepository.findOneParty(pId);
             findParty.updateMinAbility(ability);
         }
     }
 
     // 파티 예정 시간 수정
     public void updatePartyReservedTime(Long pId, Instant time) {
-        Party findParty = partyRepository.findOne(pId);
+        Party findParty = partyRepository.findOneParty(pId);
         findParty.updateReservedTime(time);
     }
 
 
         /* 파티 검색 */
-    public Party findByPartyId(Long partyId) { return partyRepository.findOne(partyId); }
+    public Party findByPartyId(Long partyId) { return partyRepository.findOneParty(partyId); }
 
 
     // 모든 파티 검색
@@ -190,6 +218,11 @@ public class PartyService {
     // find all participant by party id
     public List<PartyParticipant> findAllParticipant(Long pId) {
         return partyRepository.findAllParticipant(pId);
+    }
+
+    // find all specific status' party participants on specific party
+    public List<PartyParticipant> findPartyParticipantWithStatus(Long pId, ParticipantStatus status) {
+        return partyRepository.findPartyParticipantWithStatus(pId, status);
     }
 
 
