852b6736efc63da51d8080ffbd7c693796fc1bb0
Joonwoo Park <93jpark@gmail.com>
2022-10-19 13:02:25 +0900

    implemented Common ExcpeionAdvice

--- src/main/java/ouraid/ouraidback/api/advice/CommonExceptionAdvice.java
+++ src/main/java/ouraid/ouraidback/api/advice/CommonExceptionAdvice.java
@@ -1,2 +1,19 @@
-package ouraid.ouraidback.api.advice;public class CommonExceptionAdvice {
+package ouraid.ouraidback.api.advice;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+@ControllerAdvice
+@Slf4j
+public class CommonExceptionAdvice {
+    @ExceptionHandler(NoHandlerFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity handle404(NoHandlerFoundException e) {
+        return ResponseEntity.notFound().build();
+    }
 }
